fdc2fadd6b92a3a9b69232d1662354426f8a4968
tykyphatky <gg.fctaiphat@yahoo.com>
2025-06-17 22:05:33 +0700

    z

--- BE/BlogApi/BlogApi/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
+++ BE/BlogApi/BlogApi/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109a4b15344a9f7cf5fc9723f7559c393a17b28e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0eb3aee7fa6238da3ce27731bec58519568893")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
